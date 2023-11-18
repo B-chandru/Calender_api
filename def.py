f2f164cb64af68eeec281c566a599d565a850b52
Chandru <65664480+B-chandru@users.noreply.github.com>
2023-11-18 12:40:21 +0530

    Add files via upload

---
+++ def.py
@@ -0,0 +1,29 @@
+import calendar;
+from flask import Flask, render_template, request;
+app = Flask(__name__)
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+
+@app.route('/calender_details',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      year = request.form['year']  
+      a =calendar.calendar(int(year)).split('\n')
+      b =calendar.month(2001,1)
+      return a
+
+@app.route('/calender_details/<year>',methods = ['POST', 'GET'])
+def calender_result(year):
+      year_details =calendar.calendar(int(year)).split('\n')
+      return year_details
+
+@app.route('/Calender_Month_details/<year>/<month>',methods = ['POST', 'GET'])
+def month_result(year,month):
+      month_details =calendar.month(int(year),int(month)).split('\n')
+      return month_details
+
+if __name__ == '__main__':
+   app.run(debug = True)
